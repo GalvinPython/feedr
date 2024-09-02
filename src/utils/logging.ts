880bd2b86795cb547101c99e5355deff34f5b6f2
GalvinPython <77013913+GalvinPython@users.noreply.github.com>
2024-09-02 14:47:02 +0100

    added links to twitch notifications

---
+++ src/utils/logging.ts
@@ -0,0 +1,10 @@
+// WIP
+// TODO: Work on
+
+export async function logMessageYouTube(channelName: string, channelId: string, guildName: string, guildId: string) {
+	console.log(`❤️ [YouTube] Sent upload message to ${guildName} (${guildId}) for channel ${channelName} (${channelId})`);
+}
+
+export async function logMessageTwitch(channelName: string, channelId: string, guildName: string, guildId: string) {
+	console.log(`💜 [Twitch] Sent live message to ${guildName} (${guildId}) for channel ${channelName} (${channelId})`);
+}
