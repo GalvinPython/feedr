6458eb5c74bfbd4682888e5be720f90f07a7a9fc
GalvinPython <imgalvin@pm.me>
2025-05-18 10:24:48 +0100

    feat(web): add privacy and 404 page

--- web/src/components/404.tsx
+++ web/src/components/404.tsx
@@ -1,10 +1,10 @@
 import { motion } from "framer-motion";
 
-export const LegalStuffTos = () => {
+export const _404 = () => {
     return (
         <section
             className="lg:mb-16 w-full flex flex-col justify-center items-center bg-bgDark1"
-            id="tos"
+            id="404"
         >
             <div className="shape-divider-bottom-1665696614">
                 <svg
@@ -28,36 +28,19 @@ export const LegalStuffTos = () => {
             >
                 <div className=" 2xl:w-[1150px] xl:w-[1050px]  md:w-4/5 flex justify-center bg-bgDark1 pt-12 lg:pt-24 pb-8 lg:pb-20 mx-auto flex-col">
                     <div className="w-3/4 lg:w-1/2 flex flex-col lg:mx-unset mx-auto">
-                        <span className="block-subtitle">Terms of Service</span>
+                        <span className="block-subtitle">Terms 404</span>
                         <h2 className="mt-10 mb-8 text-4xl lg:text-5xl block-big-title">
-                            Terms of Service
+                            Page Not Found
                         </h2>
                         <p className="text-secondaryText leading-loose">
-                            When using this app you agree to the following:
+                            Please go home
                         </p>
-                        <ul className="mb-16 text-secondaryText leading-loose list-disc list-inside">
-                            <li>
-                                You will not intentionally cause errors with the
-                                bot, including DDoS attacks.
-                            </li>
-                            <li>
-                                You will not misuse any bugs you find and you
-                                will report them instead.
-                            </li>
-                            <li>
-                                Any servers you use them in comply with the
-                                Discord Terms of Service.
-                            </li>
-                        </ul>
-                        <i className="text-secondaryText leading-loose">
-                            Last updated 15th August 2024
-                        </i>
                         <button
                             className="w-[210px] h-12 contained-button mr-10 "
-                            onClick={() => window.open("/invite/bot", "_blank")}
+                            onClick={() => (window.location.href = "/")}
                             aria-label="Get started"
                         >
-                            Get Started
+                            Home
                         </button>
                     </div>
                 </div>
