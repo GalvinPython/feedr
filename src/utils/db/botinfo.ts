a14ca30fe5480ca9ec2b749dd39efa9a8cc2f957
GalvinPython <imgalvin@pm.me>
2025-06-29 21:02:59 +0100

    push before merging site into dev

--- src/utils/db/botinfo.ts
+++ src/utils/db/botinfo.ts
@@ -50,3 +50,75 @@ export async function getBotInfo() {
         return null;
     }
 }
+
+export async function updateBotInfoNotifications(
+    platform: "youtube" | "bluesky" | "twitch",
+) {
+    const query = `
+        INSERT INTO bot_info_notifications (date, total_${platform})
+        VALUES (CURRENT_DATE, 1)
+        ON CONFLICT (date) DO UPDATE
+        SET total_${platform} = bot_info_notifications.total_${platform} + 1;
+    `;
+
+    try {
+        const client: PoolClient = await pool.connect();
+
+        await client.query(query, [platform]);
+
+        client.release();
+
+        console.log(`Bot info notifications updated for ${platform}`);
+    } catch (err) {
+        console.error(
+            `Error updating bot info notifications for ${platform}:`,
+            err,
+        );
+    }
+}
+
+export async function getBotInfoNotifications() {
+    const query = `SELECT * FROM bot_info_notifications`;
+
+    try {
+        const client: PoolClient = await pool.connect();
+        const result: QueryResult<any> = await client.query(query);
+
+        client.release();
+
+        return result.rows;
+    } catch (err) {
+        console.error("Error getting bot info notifications:", err);
+
+        return [];
+    }
+}
+
+export async function updateBotInfoNotificationsTimings(
+    channel_id: string,
+    time_ms: number,
+): Promise<void> {
+    const query = `
+        INSERT INTO bot_info_notifications_timings (time, channel_id, time_ms)
+        VALUES (NOW(), $1, $2)
+        ON CONFLICT (time, channel_id) DO UPDATE
+        SET time_ms = EXCLUDED.time_ms;
+    `;
+
+    try {
+        const client: PoolClient = await pool.connect();
+
+        await client.query(query, [channel_id, time_ms]);
+
+        client.release();
+
+        console.log(
+            `Bot info notifications timings updated for channel ${channel_id}`,
+        );
+    } catch (err) {
+        console.error(
+            `Error updating bot info notifications timings for channel ${channel_id}:`,
+            err,
+        );
+    }
+}
